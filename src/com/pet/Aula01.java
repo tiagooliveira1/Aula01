a3427ab280c894431daa990a1f74881d6a6ec541
Tiago  Oliveira <tiago@econtabil.com>
2017-03-02 18:01:28 -0300

    Config biblioteca

--- src/com/pet/Aula01.java
+++ src/com/pet/Aula01.java
@@ -4,7 +4,7 @@ public class Aula01 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.print("Análise Léxica!");
+		System.out.print("Teste!");
 
 
 	}
